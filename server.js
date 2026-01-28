807bcda4df86d94e9a3c91392dfccb02d2e233fc
komalk-maker <komal.k@kalkikalakar.com>
2026-01-28 20:29:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
 
 // File upload handler
 const upload = multer({ dest: "uploads/" });
